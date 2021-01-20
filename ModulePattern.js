2ec858776b4ef330da1b31bde3c7d85efd03f1ea
ginolee <ginolee22@outlook.com>
2021-01-19 23:24:14 -0800

    Revealing Module Pattern

--- ModulePattern.js
+++ ModulePattern.js
@@ -71,3 +71,32 @@ var myModule = (function (jQ, _) {
 })(jQuery, _);
 
 myModule.publicMethod();
+
+// Revealing Module Pattern
+var myRevealingModule = (function () {
+
+  var privateVar = "Ben Cherry",
+    publicVar = "Hey there!";
+
+  function privateFunction() {
+    console.log("Name:" + privateVar);
+  }
+
+  function publicSetName(strName) {
+    privateVar = strName;
+  }
+
+  function publicGetName() {
+    privateFunction();
+  }
+
+  // Reveal public pointers to
+  // private functions and properties
+  return {
+    setName: publicSetName,
+    greeting: publicVar,
+    getName: publicGetName
+  };
+})();
+
+myRevealingModule.setName("Paul Kinlan");
